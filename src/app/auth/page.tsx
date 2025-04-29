c1e886931eb62b362838cebdcdfc45d6146c7f20
dilan-wil <nuadjedilan@gmail.com>
2025-04-29 17:31:53 +0100

    Update page.tsx

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -67,7 +67,7 @@ export default function AuthPage() {
         <div className="container mx-auto px-4 py-3 flex justify-between items-center">
           <Link href="/" className="flex items-center space-x-2">
             {/* <Droplet className="h-6 w-6 text-primary" /> */}
-            <span className="font-bold text-xl">D.H Law Firm</span>
+            <span className="font-bold text-xl">D. Happi Avocats</span>
           </Link>
           <Link
             href="/"
